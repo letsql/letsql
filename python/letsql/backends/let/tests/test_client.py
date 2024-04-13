b0b038ec2c3c7f710d36121e71217965e435ecc2
dlovell <dlovell@gmail.com>
2024-04-13 15:38:06 -0400

    test: add client tests for new register types

---
+++ python/letsql/backends/let/tests/test_client.py
@@ -0,0 +1,23 @@
+import pyarrow as pa
+
+import letsql as ls
+from letsql.backends.let.tests.conftest import assert_frame_equal
+
+
+def test_register_record_batch_reader(alltypes_df):
+    con = ls.connect()
+    t = con.register(alltypes_df, "alltypes")
+    record_batch_reader = t.to_pyarrow_batches()
+    t2 = con.register(record_batch_reader, "t2")
+    actual = t2.execute()
+
+    assert isinstance(record_batch_reader, pa.RecordBatchReader)
+    assert_frame_equal(actual, alltypes_df)
+
+
+def test_register_expr(alltypes_df):
+    con = ls.connect()
+    t = con.register(alltypes_df, "alltypes")
+    t2 = con.register(t, "alltypes2")
+    actual = t2.execute()
+    assert_frame_equal(actual, alltypes_df)
