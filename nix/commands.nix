14a304758766263622c7ed2810bec0aa043f9fad
Dan Lovell <dlovell@gmail.com>
2024-05-17 08:11:42 -0400

    dlovell/nix commands (#42)
    * fix(nix): letsql-ensure-download-data: make download conditional
    * feat(nix): add command to deal with docker newgrp issue
    * fix(nix): remove destructive `git clean` invocation
    * feat(nix): add command letsql-git-fetch-origin-pull
    * fix(nix): letsql-pytest: enable passing args
    * feat(nix): add letsql-git-config-blame-ignore-revs

--- nix/commands.nix
+++ nix/commands.nix
@@ -4,13 +4,7 @@
     set -eux
 
     # see https://docs.pytest.org/en/latest/explanation/pythonpath.html#import-mode-importlib
-    ${python}/bin/python -m pytest --import-mode=importlib
-  '';
-
-  letsql-clean = pkgs.writeShellScriptBin "letsql-clean" ''
-    set -eux
-
-    ${pkgs.git}/bin/git clean --force -dx --exclude 'ci/ibis-testing-data'
+    ${python}/bin/python -m pytest --import-mode=importlib "''${@}"
   '';
 
   letsql-fmt = pkgs.writeShellScriptBin "letsql-fmt" ''
@@ -55,8 +49,9 @@
   '';
 
   letsql-ensure-download-data = pkgs.writeShellScriptBin "letsql-ensure-download-data" ''
-    repo_dir=$(realpath $(git rev-parse --git-dir)/..)
-    if [ ! -d "$repo_dir/ci/ibis-testing-data" ]; then
+    git_dir=$(git rev-parse --git-dir 2>/dev/null) || exit
+    repo_dir=$(realpath "$git_dir/..")
+    if [ "$(dirname "$repo_dir")" = "letsql" ] && [ ! -d "$repo_dir/ci/ibis-testing-data" ]; then
       ${letsql-download-data}/bin/letsql-download-data
     fi
   '';
@@ -68,9 +63,32 @@
     docker compose up --build --wait ''${backends[@]}
   '';
 
+  letsql-newgrp-docker-compose-up = pkgs.writeShellScriptBin "letsql-newgrp-docker-compose-up" ''
+    set -eux
+
+    newgrp docker <<<"${letsql-docker-compose-up}/bin/letsql-docker-compose-up ''${@}"
+  '';
+
+  letsql-git-fetch-origin-pull = pkgs.writeShellScriptBin "letsql-git-fetch-origin-pull" ''
+    set -eux
+
+    PR=$1
+    branchname="origin-pr-$PR"
+    git fetch origin pull/$PR/head:$branchname
+  '';
+
+  letsql-git-config-blame-ignore-revs = pkgs.writeShellScriptBin "letsql-git-config-blame-ignore-revs" ''
+    set -eux
+
+    # https://black.readthedocs.io/en/stable/guides/introducing_black_to_your_project.html#avoiding-ruining-git-blame
+    ignore_revs_file=''${1:-.git-blame-ignore-revs}
+    ${pkgs.git}/bin/git config blame.ignoreRevsFile "$ignore_revs_file"
+  '';
+
   letsql-commands = {
-    inherit letsql-pytest letsql-clean letsql-fmt letsql-lint letsql-ensure-download-data letsql-docker-compose-up;
+    inherit letsql-pytest letsql-fmt letsql-lint letsql-ensure-download-data letsql-docker-compose-up letsql-newgrp-docker-compose-up letsql-git-fetch-origin-pull letsql-git-config-blame-ignore-revs;
   };
+
   letsql-commands-star = pkgs.buildEnv {
     name = "letsql-commands-star";
     paths = builtins.attrValues letsql-commands-star;
