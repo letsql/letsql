5bf43cada8d7615bf9fd856c1e28fd98b4af5ac2
Daniel Mesejo <mesejoleon@gmail.com>
2024-07-22 13:03:06 +0200

    feat: use options.backend as ParquetCacheStorage's default backend

--- python/letsql/common/tests/test_cache.py
+++ python/letsql/common/tests/test_cache.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
-import ibis
+import letsql as ls
 import pytest
 
+import letsql.backends.let
 from letsql.common.caching import ParquetCacheStorage
 
 
@@ -14,10 +15,10 @@ def parquet_dir():
 
 
 def test_put_get_drop(tmp_path, parquet_dir):
-    batting_path = parquet_dir.joinpath("astronauts.parquet")
+    astronauts_path = parquet_dir.joinpath("astronauts.parquet")
 
-    con = ibis.datafusion.connect()
-    t = con.register(batting_path, table_name="astronauts")
+    con = ls.datafusion.connect()
+    t = con.register(astronauts_path, table_name="astronauts")
 
     storage = ParquetCacheStorage(path=tmp_path, source=con)
     put_node = storage.put(t, t.op())
@@ -29,3 +30,19 @@ def test_put_get_drop(tmp_path, parquet_dir):
     storage.drop(t)
     with pytest.raises(KeyError):
         storage.get(t)
+
+
+def test_default_connection(tmp_path, parquet_dir):
+    batting_path = parquet_dir.joinpath("astronauts.parquet")
+
+    con = ls.datafusion.connect()
+    t = con.register(batting_path, table_name="astronauts")
+
+    storage = ParquetCacheStorage(path=tmp_path)
+    storage.put(t, t.op())
+
+    get_node = storage.get(t)
+    assert get_node is not None
+    assert get_node.source.name == "let"
+    assert letsql.options.backend is not None
+    assert get_node.to_expr().execute is not None
