bdad8b96bbaa3a90e9991a49957062b5f1b660d6
Daniel Mesejo <mesejoleon@gmail.com>
2024-08-14 11:31:50 +0200

    chore: remove unused function

--- python/letsql/optimizer/tests/test_predict.py
+++ python/letsql/optimizer/tests/test_predict.py
@@ -1,6 +1,5 @@
 import json
 import os
-import struct
 from itertools import chain
 
 import pandas as pd
@@ -46,50 +45,6 @@ def train_xgb(
 
     # Return the trained model
     return model
-
-
-def convert_and_save(input_model, output_file):
-    model = xgb.Booster()
-    model.load_model(input_model)
-    tmp_file = output_file + ".gbdt_rs.mid"
-    # extract base score
-    try:
-        with open(input_model, "rb") as f:
-            model_format = struct.unpack("cccc", f.read(4))
-            model_format = b"".join(model_format)
-            if model_format == b"bs64":
-                print("This model type is not supported")
-            elif model_format != "binf":
-                f.seek(0)
-            base_score = struct.unpack("f", f.read(4))[0]
-    except Exception as e:
-        print("error: ", e)
-        return 1
-
-    if os.path.exists(tmp_file):
-        print(
-            "Intermediate file %s exists. Please remove this file or change your output file path"
-            % tmp_file
-        )
-        return 1
-
-    # dump json
-    model.dump_model(tmp_file, dump_format="json")
-
-    # add base score to json file
-    try:
-        with open(output_file, "w") as f:
-            f.write(repr(base_score) + "\n")
-            with open(tmp_file) as f2:
-                for line in f2.readlines():
-                    f.write(line)
-    except Exception as e:
-        print("error: ", e)
-        os.remove(tmp_file)
-        return 1
-
-    os.remove(tmp_file)
-    return 0
 
 
 def model_features(path):
