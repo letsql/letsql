74a4d7322db2f254cf766a7d16c379a039c99897
Daniel Mesejo <mesejoleon@gmail.com>
2024-08-20 22:35:23 +0200

    chore: add additional verification

--- examples/multi_engine.py
+++ examples/multi_engine.py
@@ -1,5 +1,6 @@
 import letsql as ls
 
+# this example also shows the no need for registering tables or expression directly in LETSQL
 
 pg = ls.postgres.connect_examples()
 db = ls.duckdb.connect()
@@ -10,13 +11,15 @@ awards_players = db.register(
     ls.config.options.pins.get_path("awards_players"),
     table_name="awards_players",
 )
-
-
 left = batting[batting.yearID == 2015]
 right = awards_players[awards_players.lgID == "NL"].drop("yearID", "lgID")
 expr = left.join(right, ["playerID"], how="semi")[["yearID", "stint"]]
+
+
 result = expr.execute()
-
-
+print(result)
+result = expr.to_pyarrow()
+print(result)
+result = expr.to_pyarrow_batches().read_pandas()
 print(result)
 print(tuple(dt.args for dt in expr.ls.native_dts))
