e983d0025c9a90ec4068e5ba975ce183583223ad
Dan Lovell <dlovell@gmail.com>
2024-10-01 09:46:15 -0400

    fix maturin override (#277)
    * fix(nix): update expected output hash for gbdt
    
    * fix(nix): use toolchain in maturinOverride

--- nix/letsql.nix
+++ nix/letsql.nix
@@ -26,7 +26,7 @@ let
       fileset = unions [ rustSrcSet pySrcSet ];
     };
     toolchain = pkgs.rust-bin.fromRustupToolchainFile (append src "rust-toolchain.toml");
-    craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+    craneLib = (crane.mkLib pkgs).overrideToolchain (p: toolchain);
     wheelName = let
       inherit (craneLib.crateNameFromCargoToml { inherit cargoToml; }) pname version;
       wheelTail = {
@@ -104,12 +104,13 @@ let
       cargoDeps = importCargoLock {
         lockFile = "${old.src}/Cargo.lock";
         outputHashes = {
-          "gbdt-0.1.3" = "sha256-oEiCWgbu9FBJt+5ceGTt/OCqbx1UI24dXOUEFy0hZoU=";
+          "gbdt-0.1.3" = "sha256-f2uqulFSNGwrDM7RPdGIW11VpJRYexektXjHxTJHHmA=";
         };
       };
       nativeBuildInputs = (old.nativeBuildInputs or []) ++ [
         cargoSetupHook
         maturinBuildHook
+        toolchain
       ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
         pkgs.libiconv
       ];
