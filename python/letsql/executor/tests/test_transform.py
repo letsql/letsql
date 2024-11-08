eda3b14a93b13efc909a4dfcc681016fd909ed3d
Daniel Mesejo <mesejoleon@gmail.com>
2024-11-08 21:02:22 +0100

    feat: code for remote tables

--- python/letsql/executor/tests/test_transform.py
+++ python/letsql/executor/tests/test_transform.py
@@ -4,6 +4,9 @@ from letsql.executor.transform import transform_plan
 
 import pandas as pd
 
+import letsql as ls
+from letsql.expr.relations import into_backend
+
 
 def test_transform_plan(pg):
     t = pg.table("batting")
@@ -22,3 +25,22 @@ def test_transform_plan(pg):
     result = node.to_expr().execute()
     assert isinstance(result, pd.DataFrame)
     assert len(result) == 15
+
+
+def test_transform_into_backend_join(pg):
+    con = ls.connect()
+    t = into_backend(pg.table("batting"), con, "ls_batting")
+
+    expr = (
+        t.join(t, "playerID")
+        .order_by("playerID", "yearID_right")
+        .select(player_id="playerID", year_id="yearID_right")
+        .limit(15)
+    )
+
+    plan = segment(expr.op())
+    node = transform_plan(plan)
+
+    actual = node.to_expr().execute()
+    assert isinstance(actual, pd.DataFrame)
+    assert len(actual) == 15
