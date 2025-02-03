9c8c2c6b99d01470c26463009c45ee571ad63a02
Hussain Sultan <hussainz@gmail.com>
2025-02-03 13:36:38 -0500

    chore(examples): add quickgrove example (#499)

---
+++ examples/quickgrove_udf.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import pandas as pd
+
+import letsql as ls
+from letsql.expr.ml import make_quickgrove_udf, rewrite_quickgrove_expr
+from letsql.expr.relations import into_backend
+
+
+model_name = "diamonds-model"
+model_path = Path(ls.options.pins.get_path(model_name))
+new_model_path = model_path.parent / "diamonds_model.json"
+# cannot have hyphen in the model name see #498
+model_path.replace(new_model_path)
+
+# source backend
+pg = ls.postgres.connect_examples()
+# local backend
+con = ls.connect()
+# load xgboost json model
+model = make_quickgrove_udf(new_model_path)
+
+t = (
+    into_backend(pg.tables["diamonds"], con, "diamonds")
+    .mutate(pred=model.on_expr)
+    .filter(ls._.carat < 1)
+    .select(ls._.pred)
+)
+
+t_pruned = rewrite_quickgrove_expr(t)
+
+original = ls.execute(t)
+pruned = ls.execute(t_pruned)
+
+pd.testing.assert_frame_equal(original, pruned, rtol=3)
