868010e9284213d800013fd03cb9fbd671137aca
Dan Lovell <dlovell@gmail.com>
2025-02-07 09:24:06 -0500

    fix/nix-commands (#513)
    * fix(nix): allow running from subdir of required dir
    
    * fix(nix): letsql-maturin-build: ensure correct cwd
    
    * cln(envrc): remove poetry related lines

--- nix/commands.nix
+++ nix/commands.nix
@@ -6,10 +6,12 @@ let
 
     # see https://docs.pytest.org/en/latest/explanation/pythonpath.html#import-mode-importlib
     required_dir="$(git rev-parse --show-toplevel)/python/letsql"
-    if [ "$(pwd)" != "$required_dir" ]; then
-      echo "must run from $required_dir"
-      exit 1
-    fi
+
+    case $PWD/ in
+      "$required_dir"*) true;;
+      *) echo "must run from inside $required_dir"; exit 1
+    esac
+
     ${python}/bin/python -m pytest --import-mode=importlib "''${@}"
   '';
 
@@ -93,6 +95,8 @@ let
 
   letsql-maturin-build = pkgs.writeShellScriptBin "letsql-maturin-build" ''
     set -eux
+    repo_dir=$(git rev-parse --show-toplevel)
+    cd "$repo_dir"
     ${python}/bin/maturin build --release
   '';
 
